f586310ba577e23c03c95c431b817348ba0d2c8e
ViratLee <lneung@yahoo.com>
2015-08-07 18:46:25 +0700

    change json format.

--- whupserver/whupsever.js
+++ whupserver/whupsever.js
@@ -2,11 +2,7 @@ var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  
-  //var jstr = "{\"employees\":[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\", \"lastName\":\"Jones\"}]}";
-  var jstr = "[{\"create\":\"3\", \"indexed\":\"4\",\"ended\":\"5\"}]";
-  
-  //res.send(JSON.stringify(jstr));
+  var jstr = "{\"data\":[[\"create\",3],[ \"indexed\",4],[\"ended\",5]]}";
   res.send(jstr);
   });
 var server = app.listen(3000, function () {
