2222afa90d0644127cbbd5ef310a8154b8314dcd
Nitisak <nitisak.mooltreesri@gmail.com>
2015-08-07 18:41:33 +0700

    add whupserver

--- whupserver/public/whatup.js
+++ whupserver/public/whatup.js
@@ -1,10 +1,53 @@
 var express = require('express');
 var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});*/
+
 app.get('/', function (req, res) {
   //var jstr = "{\"employees\":[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\", \"lastName\":\"Jones\"}]}";
   var jstr = "{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\", \"lastName\":\"Jones\"}";
   res.send(JSON.stringify(jstr));
   });
+  
+  
+  app.get('/', function (req, res) {
+  var url="www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new";
+  res.send(JSON.stringify(jstr));
+  });
+  
+  
+  
+  
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
