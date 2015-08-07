c679f6373db7a6bab0164512e1ee9891a9de196d
ViratLee <lneung@yahoo.com>
2015-08-07 18:02:51 +0700

    add  set header Access-control-all-origin

--- whupserver/whupsever.js
+++ whupserver/whupsever.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  var jstr = "{\"employees\":[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\", \"lastName\":\"Jones\"}]}";
-  res.send(JSON.stringify(jstr));
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
   
+  //var jstr = "{\"employees\":[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\", \"lastName\":\"Jones\"}]}";
+  var jstr = "[{\"create\":\"3\", \"indexed\":\"4\",\"ended\":\"5\"}]";
+  
+  //res.send(JSON.stringify(jstr));
+  res.send(jstr);
+  });
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
