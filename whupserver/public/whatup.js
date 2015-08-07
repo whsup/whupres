f61fd2df12ca3a75ce3392087c78012591b8b81b
ViratLee <lneung@yahoo.com>
2015-08-07 17:09:26 +0700

    update

---
+++ whupserver/public/whatup.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  //var jstr = "{\"employees\":[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\", \"lastName\":\"Jones\"}]}";
+  var jstr = "{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\", \"lastName\":\"Jones\"}";
+  res.send(JSON.stringify(jstr));
+  });
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
