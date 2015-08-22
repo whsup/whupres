8e892c40342896c9089b25b2cbaf130e4d0ed70e
Nitisak <nitisak.mooltreesri@gmail.com>
2015-08-22 15:15:55 +0700

    Add getStockData

--- whupserver/whupsever.js
+++ whupserver/whupsever.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+
+
 app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   var jstr = "{\"data\":[[\"create\",3],[ \"indexed\",4],[\"ended\",5]]}";
   res.send(jstr);
   });
+
+
+app.get('/getStockData',function(req,res){
+
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
