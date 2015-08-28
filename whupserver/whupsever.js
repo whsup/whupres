45ae29ce031ec1abb8638260a2bdbe4b94b438a4
Nitisak <nitisak.mooltreesri@gmail.com>
2015-08-28 15:12:45 +0700

    Add steps in TEST Data

--- whupserver/whupsever.js
+++ whupserver/whupsever.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  var jstr = "{\"data\":[[\"create\",3],[ \"indexed\",4],[\"ended\",5]]}";
+  var jstr = "{\"data\":[[\"Backlog\",3],[ \"PROCESS\",4],[\"REVIEW\",5],[\"REWORK\",5],[\"TEST\",5],[\"FIX\",5]]}";
   res.send(jstr);
   });
 
