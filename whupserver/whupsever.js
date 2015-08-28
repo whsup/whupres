e1143189c82e48ff7643c733925e27ab3dcc76eb
Nitisak <nitisak.mooltreesri@gmail.com>
2015-08-28 17:38:58 +0700

    Add request stock data

--- whupserver/whupsever.js
+++ whupserver/whupsever.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var http = require('http');
 
 app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,9 +10,29 @@ app.get('/', function (req, res) {
 
 
 app.get('/getStockData',function(req,res){
-	res.setHeader('Access-Control-Allow-Origin', '*');
-  var jstr = "{\"data\":[[\"Backlog2\",3],[ \"PROCESS\",4],[\"REVIEW\",5],[\"REWORK\",5],[\"TEST\",5],[\"FIX\",5]]}";
-  res.send(jstr);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  var options = {
+    host : 'www.highcharts.com',
+    path : '/samples/data/jsonp.php?filename=range.json',
+    port : 80,
+    method : 'GET'
+  }
+
+  var request = http.request(options, function(response){
+  console.log("data="+response+ "end");
+    var body = ""
+    response.on('data', function(data) {
+      body += data;
+    });
+    response.on('end', function() {
+      res.send(JSON.parse(body));
+    });
+  });
+  request.on('error', function(e) {
+    console.log('Problem with request: ' + e.message);
+  });
+  request.end();
+  //res.send(jstr);
 });
 
 app.get('/getAWDData',function(req,res){
